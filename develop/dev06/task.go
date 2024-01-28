34bba9105b224f5f214b780048ee8a2812abc59d
Kirill <fedyushinka@student.bmstu.ru>
2024-01-28 20:45:43 +0300

    Develop 6 done

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +21,73 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Flags - структура с флагами
+type Flags struct {
+	F int
+	D string
+	S bool
+}
 
+var flags Flags
+
+func getPrettyString(lines []string) string {
+	var result string
+	for _, v := range lines {
+		result += v + "\n"
+	}
+	if len(result) != 0 {
+		result = result[:len(result)-1]
+	}
+	return result
+}
+
+func cutLines(lines []string) []string {
+	var result []string
+	// Проходимся по всем строкам
+	for _, line := range lines {
+		// Создаем мапу, где ключ - номер колонки, а значение - слово
+		mapCols := make(map[int]string)
+		lineCols := strings.Split(line, flags.D)
+
+		// Заполняем мапу
+		for i, v := range lineCols {
+			mapCols[i+1] = v
+		}
+
+		// Если в строке больше одной колонки и установлен флаг s (то есть в строке были разделители)
+		// или если флага s нет
+		if (flags.S && len(mapCols) > 1) || !flags.S {
+			v, ok := mapCols[flags.F]
+			if ok {
+				result = append(result, v)
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	// Создаем и парсим флаги
+	flag.IntVar(&flags.F, "f", -1, "Select fields (columns)")
+	flag.StringVar(&flags.D, "d", " ", "Use a different delimiter")
+	flag.BoolVar(&flags.S, "s", false, "Only delimited lines")
+	flag.Parse()
+
+	var lines []string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Читаем строки, пока не введем пустую строку или CTRL+D
+	for {
+		scanner.Scan()
+		text := scanner.Text()
+		if len(text) != 0 {
+			lines = append(lines, text)
+		} else {
+			fmt.Println("Input stopped")
+			break
+		}
+	}
+
+	result := cutLines(lines)
+	fmt.Println(getPrettyString(result))
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,77 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCutFlagF(t *testing.T) {
+	lines := []string{
+		"111 222 333 444",
+		"55 66 77",
+		"8 9 10 11 12",
+		"13 14 15",
+		"16",
+	}
+	want := []string{
+		"222",
+		"66",
+		"9",
+		"14",
+	}
+	flags.F = 2
+	flags.D = " "
+	result := cutLines(lines)
+	flags.F = -1
+	if !reflect.DeepEqual(result, want) {
+		t.Fatalf("Case:\n%s\nWant:\n%s\nGot:\n%s", getPrettyString(lines), getPrettyString(want), getPrettyString(result))
+	}
+}
+
+func TestCutFlagD(t *testing.T) {
+	lines := []string{
+		"111.222.333.444",
+		"55.66.77",
+		"8.9.10.11.12",
+		"13.14.15",
+		"16",
+	}
+	want := []string{
+		"333",
+		"77",
+		"10",
+		"15",
+	}
+	flags.F = 3
+	flags.D = "."
+	result := cutLines(lines)
+	flags.F = -1
+	flags.D = " "
+	if !reflect.DeepEqual(result, want) {
+		t.Fatalf("Case:\n%s\nWant:\n%s\nGot:\n%s", getPrettyString(lines), getPrettyString(want), getPrettyString(result))
+	}
+}
+
+func TestCutFlagS(t *testing.T) {
+	lines := []string{
+		"111 222 333 444",
+		"55.66.77",
+		"8 9 10 11 12",
+		"13 14.15",
+		"16",
+	}
+	want := []string{
+		"66",
+		"15",
+	}
+	flags.F = 2
+	flags.D = "."
+	flags.S = true
+	result := cutLines(lines)
+	flags.F = -1
+	flags.D = " "
+	flags.S = false
+	if !reflect.DeepEqual(result, want) {
+		t.Fatalf("Case:\n%s\nWant:\n%s\nGot:\n%s", getPrettyString(lines), getPrettyString(want), getPrettyString(result))
+	}
+}
